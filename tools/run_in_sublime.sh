fdc7afc41886c37a23c06a57aa56be47c0370a1b
fleet30 <mazb@soulgame.com>
2014-12-16 15:30:27 +0800

    测试luac，可以编译，load有问题

--- tools/run_in_sublime.sh
+++ tools/run_in_sublime.sh
@@ -8,4 +8,8 @@ rm -r $1/runtime/mac/vineSample' 'Mac.app/Contents/Resources/res
 cp -R $1/res $1/runtime/mac/vineSample' 'Mac.app/Contents/Resources/
 cp -R $1/src $1/runtime/mac/vineSample' 'Mac.app/Contents/Resources/
 
+#key: 2dxLua
+#sign: XXTEA
+#/Users/maAya/SDK/cocos2d-x-3.3rc0/tools/cocos2d-console/bin/cocos luacompile -s $1/src -d $1/runtime/mac/vineSample' 'Mac.app/Contents/Resources/src -e True -k 2dxLua -b XXTEA
+
 $1/runtime/mac/vineSample' 'Mac.app/Contents/MacOS/vineSample' 'Mac
